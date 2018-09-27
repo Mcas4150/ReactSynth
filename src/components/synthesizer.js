6bf436821f15e48a2bee5538b697ec3d7906c903
Michael Cassidy <mikecassidy0000@gmail.com>
2018-09-27 13:21:42 -0400

    effects

--- src/components/synthesizer.js
+++ src/components/synthesizer.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Tone from "tone";
+import "./synthesizer.css";
 // import Envelopes from "./envelopes";
 
 export default class Synthesizer extends Component {
@@ -11,19 +12,33 @@ export default class Synthesizer extends Component {
       sustain: 0.1,
       release: 0.9,
       playing: false,
-      frequency: "C4"
+      frequency: "C5",
+      delayTime: "16n",
+      feedback: 0.2,
+      roomSize: 0.9,
+      dampening: 3000
     };
+
+    this.freeverb = new Tone.Freeverb({
+      dampening: this.state.dampening,
+      roomSize: this.state.roomSize
+    }).toMaster();
+
+    this.pingPong = new Tone.PingPongDelay({
+      delayTime: this.state.delayTime,
+      feedback: this.state.feedback
+    }).connect(this.freeverb);
 
     this.envelope = new Tone.AmplitudeEnvelope({
       attack: this.state.attack,
       decay: this.state.decay,
       sustain: this.state.sustain,
       release: this.state.release
-    }).toMaster();
+    }).connect(this.pingPong);
 
     this.tone = new Tone.Oscillator({
       frequency: this.state.frequency,
-      type: "sine",
+      type: "square",
       volume: -6
     })
       .connect(this.envelope)
@@ -36,6 +51,10 @@ export default class Synthesizer extends Component {
     this.handleDecay = this.handleDecay.bind(this);
     this.handleSustain = this.handleSustain.bind(this);
     this.handleRelease = this.handleRelease.bind(this);
+    this.handleFeedback = this.handleFeedback.bind(this);
+    this.handleDelay = this.handleDelay.bind(this);
+    this.handleRoomSize = this.handleRoomSize.bind(this);
+    this.handleDampening = this.handleDampening.bind(this);
 
     // this.handleRelease = this.handleRelease.bind(this);
 
@@ -60,6 +79,26 @@ export default class Synthesizer extends Component {
     this.setState({ release: e.target.value });
   }
 
+  handleFeedback(e) {
+    this.envelope.feedback = e.target.value;
+    this.setState({ feedback: e.target.value });
+  }
+
+  handleDelay(e) {
+    this.envelope.delay = e.target.value;
+    this.setState({ delay: e.target.value });
+  }
+
+  handleRoomSize(e) {
+    this.envelope.roomSize = e.target.value;
+    this.setState({ roomSize: e.target.value });
+  }
+
+  handleDampening(e) {
+    this.envelope.dampening = e.target.value;
+    this.setState({ dampening: e.target.value });
+  }
+
   handleNote(e) {
     // this.tone.frequency = e.target.value;
     this.setState({ frequency: e.target.value });
@@ -68,6 +107,7 @@ export default class Synthesizer extends Component {
   startNote(e) {
     // this.tone.frequency = e.target.value;
     // this.setState({ frequency: e.target.value });
+    this.setState({ frequency: e.target.value });
     this.envelope.triggerAttack();
   }
 
@@ -77,7 +117,7 @@ export default class Synthesizer extends Component {
 
   render() {
     return (
-      <div>
+      <div className="container">
         <button
           onMouseDown={this.startNote}
           onMouseUp={this.stopNote}
@@ -102,50 +142,104 @@ export default class Synthesizer extends Component {
         >
           E4
         </button>
-        <h3>Attack</h3>
-        <input
-          type="range"
-          min="0.1"
-          max="1"
-          value={this.state.attack}
-          onChange={this.handleAttack}
-          className="slider"
-          id="Attack"
-          step="0.1"
-        />
-        <h3>Decay</h3>
-        <input
-          type="range"
-          min="0.1"
-          max="1"
-          value={this.state.decay}
-          onChange={this.handleDecay}
-          className="slider"
-          id="Decay"
-          step="0.1"
-        />
-        <h3>Sustain</h3>
-        <input
-          type="range"
-          min="0.1"
-          max="1"
-          value={this.state.sustain}
-          onChange={this.handleSustain}
-          className="slider"
-          id="Sustain"
-          step="0.1"
-        />
-        <h3>Release</h3>
-        <input
-          type="range"
-          min="0.1"
-          max="1"
-          value={this.state.release}
-          onChange={this.handleRelease}
-          className="slider"
-          id="Release"
-          step="0.1"
-        />
+        <div className="adsr row">
+          <div className="attack col-sm-6 col-md-3">
+            <h3>Attack</h3>
+            <input
+              type="range"
+              min="0.1"
+              max="1"
+              value={this.state.attack}
+              onChange={this.handleAttack}
+              className="slider"
+              id="Attack"
+              step="0.1"
+            />
+          </div>
+          <div className="decay col-sm-6 col-md-3">
+            <h3>Decay</h3>
+            <input
+              type="range"
+              min="0.1"
+              max="1"
+              value={this.state.decay}
+              onChange={this.handleDecay}
+              className="slider"
+              id="Decay"
+              step="0.1"
+            />
+          </div>
+          <div className="sustain col-sm-6 col-md-3">
+            <h3>Sustain</h3>
+            <input
+              type="range"
+              min="0.1"
+              max="1"
+              value={this.state.sustain}
+              onChange={this.handleSustain}
+              className="slider"
+              id="Sustain"
+              step="0.1"
+            />
+          </div>
+          <div className="release col-sm-6 col-md-3">
+            <h3>Release</h3>
+            <input
+              type="range"
+              min="0.1"
+              max="1"
+              value={this.state.release}
+              onChange={this.handleRelease}
+              className="slider"
+              id="Release"
+              step="0.1"
+            />
+          </div>
+        </div>
+        <div className="pingpong">
+          <h3>Feedback</h3>
+          <input
+            type="range"
+            min="0.1"
+            max="1"
+            value={this.state.feedback}
+            onChange={this.handleFeedback}
+            className="slider"
+            id="Feedback"
+            step="0.1"
+          />
+          <h3>Delay Time</h3>
+          <select onChange={this.handleDelay}>
+            <option value="32n">32n</option>
+            <option value="16n">16n</option>
+            <option value="8n">8n</option>
+            <option value="4n">4n</option>
+          </select>
+        </div>
+        <div className="reverb">
+          <h3>RoomSize</h3>
+          <input
+            type="range"
+            min="0.1"
+            max="1"
+            value={this.state.roomSize}
+            onChange={this.handleRoomSize}
+            className="slider"
+            id="roomSize"
+            step="0.1"
+          />
+          <h3>Dampening</h3>
+          <input
+            type="range"
+            min="1"
+            max="5000"
+            value={this.state.dampening}
+            onChange={this.handleDampening}
+            className="slider"
+            id="dampening"
+            step="100"
+          />
+        </div>
       </div>
     );
   }
